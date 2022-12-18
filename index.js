f3959b6284c1f97ec845dcff075dc1254f666da5
Ayman Salah <ayman.1551.salah@gmail.com>
2022-12-18 17:30:53 +0200

    Fixed the Background Audio Problem

--- index.js
+++ index.js
@@ -11,25 +11,12 @@ submitBtn.addEventListener("click", function (event) {
   }
 });
 
-// playIntroSound();
+window.addEventListener("load", function () {
+  playIntroSound();
+});
 
-// function playIntroSound() {
-//   var audio = new Audio();
-//   audio.src = "audio/index.mp3";
-//   // when the sound has been loaded, execute your code
-//   audio.oncanplaythrough = (event) => {
-//     var playedPromise = audio.play();
-//     if (playedPromise) {
-//       playedPromise
-//         .catch((e) => {
-//           console.log(e);
-//           if (e.name === "NotAllowedError" || e.name === "NotSupportedError") {
-//             console.log(e.name);
-//           }
-//         })
-//         .then(() => {
-//           console.log("playing sound !!!");
-//         });
-//     }
-//   };
-// }
+function playIntroSound() {
+  let backGroundAudio = new Audio();
+  backGroundAudio.src = "audio/index.mp3";
+  backGroundAudio.play();
+}
